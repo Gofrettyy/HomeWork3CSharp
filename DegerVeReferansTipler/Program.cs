a9b5cc67a03faf152a8723737b6c946d0adabae3
Nazife Aliçavuş <140203411+Gofrettyy@users.noreply.github.com>
2023-11-10 00:57:04 +0300

    Add files via upload

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Hello World!");
+// int , decimal , float , double, bool = değer tip
+// array , class, interface = referans tip
+/* bellekte Stack ve Heap adında iki alan vardır. Değer tip olanlar stackte gerçekleşir..Sadece değer kopyalanır ve işi biter.
+ * Referans tip ise heapte gerçekleşiyor.
+ * 
+ */
